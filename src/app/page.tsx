332015373b0cf430f39b1481c2050226ee5aa646
kyoungjooo <leekjoo1008@gmail.com>
2025-04-14 16:31:20 +0900

    refector: 불필요한 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
-import { Pagination } from "@/components/ui/pagination";
 import React from "react";
 
 export default function Home() {
-  return (
-    <>
-      <Pagination />
-    </>
-  );
+  return <></>;
 }
