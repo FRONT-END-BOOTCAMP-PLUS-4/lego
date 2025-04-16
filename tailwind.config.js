6facb6c76710d0a2acfa1b94a25a190e54ff89c1
hoon95 <maruon95@gmail.com>
2025-04-17 00:56:55 +0900

    chore: 의존성 추가 및 tailwind 설정 업데이트(plugin: line-clamp)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+import lineClamp from "@tailwindcss/line-clamp";
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -12,19 +15,6 @@ module.exports = {
         xl: "1272px",
       },
     },
-    // extend: {
-    //   colors: {
-    //     "blue-01": "var(--blue-01)",
-    //     "blue-02": "var(--blue-02)",
-    //     "blue-03": "var(--blue-03)",
-    //     "blue-04": "var(--blue-04)",
-    //     "blue-04": "#f7f8fc",
-    //     "gray-01": "var(--gray-01)",
-    //     "gray-02": "var(--gray-02)",
-    //     black: "var(--black)",
-    //     white: "var(--white)",
-    //   },
-    // },
   },
-  plugins: [],
+  plugins: [lineClamp],
 };
