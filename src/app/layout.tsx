59ff2ef5cbe0a21bbf234da76ead55d200ffac9f
hoon95 <maruon95@gmail.com>
2025-04-16 17:15:41 +0900

    chore: favicon, metadata 설정(client)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import CSRHead from "@/components/common/Head";
 import "./globals.css";
 
 export default function RootLayout({
@@ -9,6 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <CSRHead />
       <body>
         <Header />
         <main className="container mx-auto px-32">{children}</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Bookmark } from "lucide-react";
 
 export default function Home() {
   // DB에 카테고리 추가 시 name, href 매핑 필요
@@ -18,7 +20,7 @@ export default function Home() {
 
   return (
     <>
-      <section className="flex justify-center gap-3 mb-[150px]">
+      <section className="flex justify-center gap-[var(--space-36)] mb-[150px]">
         {category.map((item, index) => (
           <Link key={index} href={item.href} className="flex flex-col items-center gap-2">
             <Image src={item.src} alt={item.name} width={72} height={72} className="rounded-2xl" />
@@ -30,26 +32,35 @@ export default function Home() {
       {/* background layer */}
       <section className="flex gap-10 items-center justify-center pb-52">
         {/* <Image src={sampleImg} alt="placeholder" sizes="(max-width: 768px) 100vw, 33vw" /> */}
-        <div className="flex flex-col gap-6">
-          <div className="text-3xl lg:text-5xl">
+        <div className="flex flex-col justify-center items-center gap-6 text-center w-[40vw]">
+          <div className="flex flex-col items-center txt-5xl-b">
             <p>기술 면접</p>
             <p>어떻게 공부해야할까?</p>
           </div>
-          <div className="text-xl lg:text-3xl">
-            <p>설명입니당</p>
-          </div>
-          <Button variant={"round"} className="w-fit">
-            Click me!
-          </Button>
+          <p className="txt-3xl-b">잘 해야합니다</p>
+          <Link href="/questions">
+            <Button variant={"round"} className="w-fit">
+              시작하기
+            </Button>
+          </Link>
         </div>
       </section>
       <section>
-        <p className="font-bold text-2xl lg:text-4xl pb-9">많이 스크랩된 콘텐츠</p>
+        <h3 className="txt-3xl-b pb-[var(--space-36)]">많이 스크랩된 콘텐츠</h3>
         <div className="grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-2 gap-6">
           {[...Array(4)].map((_, i) => (
-            <Card key={i} variant="default">
-              카드 {i + 1}
-            </Card>
+            <Link href={`/questions/${i + 1}`} key={i}>
+              <Card key={i} variant="default" className="flex flex-col gap-[var(--space-40)]">
+                <p className="txt-2xl-b">HTTP 메소드에 대한 설명</p>
+                <div className="flex justify-between">
+                  <Badge variant="outline">JavaScript</Badge>
+                  <div className="flex gap-[8px]">
+                    <Bookmark />
+                    <p>1000</p>
+                  </div>
+                </div>
+              </Card>
+            </Link>
           ))}
         </div>
       </section>
