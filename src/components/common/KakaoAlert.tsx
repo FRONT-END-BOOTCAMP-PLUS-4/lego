dfa8794b6b58797a4fb99839bf2ca5960754ba02
hoon95 <maruon95@gmail.com>
2025-04-19 22:36:29 +0900

    style: kakao yellow 변수 추가

--- src/components/common/KakaoAlert.tsx
+++ src/components/common/KakaoAlert.tsx
@@ -17,7 +17,7 @@ export default function KakaoAlertButton() {
   return (
     <button
       onClick={handleKakao}
-      className="flex items-center gap-2 px-4 py-2 rounded-md bg-[#fee500] cursor-pointer hover:-translate-y-1 transition-transform duration-300"
+      className="flex items-center gap-2 px-4 py-2 rounded-md bg-[var(--yellow)] cursor-pointer hover:-translate-y-1 transition-transform duration-300"
     >
       <Image src="/assets/icons/kakao.svg" alt="카카오톡 아이콘" width={20} height={20} />
       <span>알림 받기</span>
