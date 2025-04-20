92852cb2f46ef42a24e6701e1595842135a2cc24
kyoungjooo <leekjoo1008@gmail.com>
2025-04-21 01:09:09 +0900

    chore: react-markdown 설치

--- src/app/questions/[questionid]/componsts/QuestionSolution.tsx
+++ src/app/questions/[questionid]/componsts/QuestionSolution.tsx
@@ -1,9 +1,29 @@
-export default function QuestionSolution({ solution }: string) {
-  return (
-    <textarea
-      className="box-border p-[24px] h-[500px] border border-[var(--blue-03)] radius mt-6 w-full resize-none focus:ring-1 focus:ring-[var(--blue-03)] focus:outline-none"
-      readOnly
-      value={"답안내용"}
-    ></textarea>
-  );
+"use client";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+interface QuestionSolutionProps {
+  solution: string;
+}
+export default function QuestionSolution({ solution }: QuestionSolutionProps) {
+  const [solutionText, setSolutionText] = useState("");
+
+  useEffect(() => {
+    if (!solution) return;
+
+    const fetchSolution = async () => {
+      try {
+        const response = await fetch(solution);
+        if (!response.ok) {
+          throw new Error("모범답안 불러오기 실패");
+        }
+        const text = await response.text();
+        setSolutionText(text);
+      } catch (error) {
+        console.error("모범답안 로딩 에러:", error);
+      }
+    };
+
+    fetchSolution();
+  }, [solution]);
+  return <ReactMarkdown>{solutionText}</ReactMarkdown>;
 }
