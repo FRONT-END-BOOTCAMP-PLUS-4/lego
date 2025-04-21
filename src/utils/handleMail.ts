ad63b10dff8f8c926d6264b246fc8e463f803594
hoon95 <maruon95@gmail.com>
2025-04-21 15:58:23 +0900

    fix: 메일받기 클릭 시 구독상태에 따른 모달창 생성

--- src/utils/handleMail.ts
+++ src/utils/handleMail.ts
@@ -4,13 +4,15 @@ import { useRouter } from "next/navigation";
 import { useProfileStore } from "@/store/useProfileStore";
 
 export function useHandleMail() {
-  const { setActiveIndex, setMailAutoToggle } = useProfileStore();
+  const { mailAutoToggle, setActiveIndex, setShowModal } = useProfileStore();
   const router = useRouter();
 
   const handleMail = () => {
-    setActiveIndex(0);
     router.push("/users");
-    setMailAutoToggle(true);
+    if (!mailAutoToggle) {
+      setActiveIndex(0);
+      setShowModal(true);
+    }
   };
 
   return handleMail;
