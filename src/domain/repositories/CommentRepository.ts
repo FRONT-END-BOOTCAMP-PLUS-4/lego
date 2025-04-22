57f4ee4391ecda7a32c8e48b5bd7390132cbc16e
이규원 <woni011420@gmail.com>
2025-04-23 00:33:16 +0900

    refactor: 댓글 개수 기능 API삭제(length 메서드로도 충분히 가능함)

--- src/domain/repositories/CommentRepository.ts
+++ src/domain/repositories/CommentRepository.ts
@@ -1,7 +1,6 @@
 import { Comment } from "@/domain/entities/Comment";
 
-export interface CommentRepository {
-  countByQuestionAndAnswer(questionId: number, answerEmail: string): Promise<number>;//댓글 개수 
+export interface CommentRepository { 
   getByQuestionAndAnswer(questionId: number, answerEmail: string): Promise<Comment[]>;//댓글 리스트
   create(comment: Comment): Promise<Comment>; //댓글 생성
   updateContent(id: number, content: string): Promise<void>;//댓글 수정
--- src/infra/repositories/supabase/SbCommentRepository.ts
+++ src/infra/repositories/supabase/SbCommentRepository.ts
@@ -5,20 +5,6 @@ import { CommentRepository } from "@/domain/repositories/CommentRepository";
 import { createClient } from "@/utils/supabase/server";
 
 export class SbCommentRepository implements CommentRepository {
-
-  //댓글 개수 출력
-  async countByQuestionAndAnswer(questionId: number, answerEmail: string): Promise<number> {
-    const supabase = await createClient();
-
-    const { count, error } = await supabase.from("comment")
-      .select("*", { count: "exact", head: true })
-      .eq("question_id", questionId)
-      .eq("answer_email", answerEmail);
-
-    if (error) throw new Error(error.message);
-    return count ?? 0;
-  }
-
   //댓글 리스트 출력
   async getByQuestionAndAnswer(questionId: number, answerEmail: string): Promise<Comment[]> {
     const supabase = await createClient();
