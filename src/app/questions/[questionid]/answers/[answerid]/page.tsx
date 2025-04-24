1bfdf6add00b2bf3be7bd00a9ac2789df614ac4d
이규원 <woni011420@gmail.com>
2025-04-24 09:48:13 +0900

    fix: 버그 수정

--- src/app/questions/[questionid]/answers/[answerid]/page.tsx
+++ src/app/questions/[questionid]/answers/[answerid]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import CommentSection from "./components/CommentSection";
-import AnswerContents from "./components/AnswerContents";
+import AnswerContent from "./components/AnswerContent";
 
 export default function AnswerDetailPage() {
   return (
     <div className="container mx-auto pt-[40px] md:px-6">
       <div className="pb-[var(--space-24)] txt-2xl-b">답변 상세보기</div>
 
-      <AnswerContents />
+      <AnswerContent />
       {/* 본문과 텍스트 사이의 패딩 */}
       <div className="mb-[150px]"></div>
       <CommentSection />
