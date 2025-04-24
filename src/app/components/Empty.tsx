00743e8206912f23dc42c9bae0b31e507512b006
kyoungjooo <leekjoo1008@gmail.com>
2025-04-24 14:37:41 +0900

    style: badge 폰트 사이즈 변경

--- src/app/components/Empty.tsx
+++ src/app/components/Empty.tsx
@@ -7,7 +7,7 @@ interface PropsType {
 }
 export default function Empty({ text }: PropsType) {
   return (
-    <Card className="text-[var(--black)] border-[var(--gray-01)] opacity-50 mx-auto w-full flex items-center justify-center h-[180px] bg-[var(--white)]">
+    <Card className="text-[var(--black)] border-[var(--gray-01)] pointer-events-none opacity-50 mx-auto w-full flex items-center justify-center h-[180px] bg-[var(--white)]">
       {text} 👻
     </Card>
   );
